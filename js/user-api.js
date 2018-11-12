c821b1b0568825b0ca3d27beefd4dc2ad7f19a8f
avanvalk <drewvv@gmail.com>
2018-11-12 15:20:36 -0800

    started user-api.js

---
+++ js/user-api.js
@@ -0,0 +1,9 @@
+
+form = document.getElementById('start-page');
+const elements = form.elements
+
+const userData = {
+    username: elements.username.value
+//    genre: elements.genre.value
+//    beatsarray: []
+}
