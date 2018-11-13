a5f9ff9e60858b1a37259ea1a962ae7d9726b8d6
Cari Palazzolo <cari.pizza@gmail.com>
2018-11-12 16:07:35 -0800

    connected start form to user-api.js, console.log test works

--- js/app.js
+++ js/app.js
@@ -1,4 +1,25 @@
-
-import userData from 'start.js';
+import html from './html.js';
+import addUserData from '.js';
 
 console.log(userData);
+
+// function makeTemplate() {
+//     return html`
+//     <form id="start-page">
+//         <label for="username">Create name:</label>
+//         <input type="text" name="username"></input>
+//         <button name="start" type="submit" id="user-submit">Submit</button>
+//     </form>
+//     `;
+// }
+
+// class App {
+//     render() {
+//         const dom = makeTemplate();
+//     }
+// }
+
+
+// const app = new App();
+// const root = document.getElementById('root');
+// root.appendChild(app.render());
--- js/user-api.js
+++ js/user-api.js
@@ -1,9 +1,24 @@
 
-form = document.getElementById('start-page');
-const elements = form.elements
+const addUserData = {
+    init: function() {
+        const form = document.getElementById('start-page');
 
-const userData = {
-    username: elements.username.value
-//    genre: elements.genre.value
-//    beatsarray: []
-}
+        form.addEventListener('submit', function(event) {
+            event.preventDefault();
+            // console.log('test');
+            const elements = form.elements;
+
+            const userData = {
+                username: elements.username.value
+                            //  genre: elements.genre.value
+                            //  beatsarray: []
+            };
+            console.log(userData.username);
+            // addUserData.init();
+        });
+    }
+};
+
+addUserData.init();
+
+// export default addUserData;
