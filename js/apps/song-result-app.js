61c3b91d7b4716019efaf2085f3184c03d19c5ae
Cari Palazzolo <cari.pizza@gmail.com>
2018-11-13 10:55:12 -0800

    tweak audio link on song-result.html

--- js/apps/song-result-app.js
+++ js/apps/song-result-app.js
@@ -4,7 +4,7 @@ import html from '../helper/html.js';
 function makeTemplate() {
     return html`
         <audio controls> <!-- loop autoplay -->
-            <source src="../assets/testaudio.mp3" type="audio/mpeg">
+            <source src="./assets/testaudio.mp3" type="audio/mpeg">
             Your browser does not support the audio tag.
         </audio>
     `;
