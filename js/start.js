b23b2061208504208a7830cbb31c1b9e91c5b20e
Phoebe LaBlonde <pmlablonde2220@gmail.com>
2018-11-12 17:58:32 -0700

    working on api

---
+++ js/start.js
@@ -0,0 +1,13 @@
+import addUserData from './user-api.js';
+
+const data = addUserData.getAll();
+
+
+
+
+const form = document.getElementById('start-page');
+
+        form.addEventListener('submit', function(event) {
+            event.preventDefault();
+            beatsarray.push()
+            //window.location(url)
