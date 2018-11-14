cc5e829db741e2220a5d53d5af87919179dff7b3
Cari Palazzolo <cari.pizza@gmail.com>
2018-11-14 10:11:39 -0800

    manually added updates from cpnov13, including function play on song-result.html

--- js/apps/sound-select-form.js
+++ js/apps/sound-select-form.js
@@ -2,23 +2,27 @@ import html from '../helper/html.js';
 import userData from '../services/user-api.js';
 
 function makeTemplate() {
-    return html`  
+    return html`
     <form id="sound-select-form">
     <h1 id = "header">CHOOSE YOUR FIRST SOUND</h1>
         <fieldset id="sample1-fieldset">
-            <label class="neon" for="sample1">Kick-drum</label>
-            <input type="checkbox" name="sample1" id="sample1" checked>
+            <label class="neon" for="sample1">Kick</label>
+            <input type="checkbox" name="sample1" id="sample1">
         </fieldset>
         <fieldset id="sample2-fieldset">
-            <label class="neon" for="sample2">Percussion</label>
-            <input type="checkbox" name="sample2" id="sample2" value="sample2" checked >
+            <label class="neon" for="sample2">Snare</label>
+            <input type="checkbox" name="sample2" id="sample2">
+        </fieldset>
+        <fieldset id="sample3-fieldset">
+            <label class="neon" for="sample3">Percussion</label>
+            <input type="checkbox" name="sample3" id="sample3">
         </fieldset>
         <button type="submit" id="button-sound-select">Submit</button>
     </form>
     `;
 }
 
-// let submit = 0;
+
 
 export default class SoundSelectForm {
     constructor(form) {
@@ -34,11 +38,11 @@ export default class SoundSelectForm {
             const elements = form.elements;
             const data = {
                 sample1: elements.sample1.checked,
-                sample2: elements.sample2.checked
+                sample2: elements.sample2.checked,
+                sample3: elements.sample3.checked
             };
 
             userData.add(data);
-            
 
             window.location = './song-result.html';
 
