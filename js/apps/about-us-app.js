e5353b09502b4d36fba9a6ab5a3654a58e1177cf
Christina T. Herlocker <cherlocker3113@gmail.com>
2018-11-15 14:54:19 -0800

    about pg tweks near completion

--- js/apps/about-us-app.js
+++ js/apps/about-us-app.js
@@ -13,22 +13,23 @@ function makeTemplate() {
             <div id="cari">
                 Cari<br>Palazzolo<br>
                 <img src="./assets/pizzacat.gif" id="caris" alt="">
-                <p>about</p>
+                <p>When Cari isn't writing code, she likes to make art and music, eat pizza, and sip delicious IPAs.</p>
             </div>
             <div id="phoebe">
                 Phoebe<br>Lablonde<br>
                 <img src="https://media.giphy.com/media/X7NBev9zI0i2s/giphy.gif" id="phoebes">
-                <p>about</p>
+                <p>When phoebe isn’t coding, she likes to:<br>
+                read, watch YouTube, and chill.</p>
             </div>
             <div id="drew">
                 Drew<br><span id=last>Van Valkenburg</span><br>
                 <img src="./assets/userprofile.gif" id="drews" alt="">
-                <p>about</p>
+                <p>Drew's texty texty texy text. Texty texty texty texty texty text!</p>
             </div>
             <div id="chris">
                 <span id= first>Christina T.</span><br>Herlocker<br>
                 <img src="./assets/snoopy.gif" id="chriss" alt="">
-                <p>about</p>
+                <p>When Chris isn't coding, she can be found reading, restoring vintage clothing, and playing video games.</p>
             </div>
         </div>
     </main>
